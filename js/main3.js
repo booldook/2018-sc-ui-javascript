69623a5be5c9afc204a1fa4407234374a531633c
Booldook <booldook@gmail.com>
2018-10-02 15:01:26 +0900

    181002-03

---
+++ js/main3.js
@@ -0,0 +1,30 @@
+/*
+변수명 규칙
+1. 숫자로 시작하지 않는다.
+2. - (하이픈)을 사용하지 않는다.
+3. 예약어(예:click, hover)를 사용하지 않는다.
+4. Camel 표기법을 가급적이면 따른다. (예: cateList, cateListArray). 
+*/
+/*
+var n = 30;
+function test() {
+	return "n은" + n + " 입니다.";
+}
+console.log(test);
+console.log(test());
+
+var n = 30;
+var test = function() {
+	return "n은" + n +" 입니다.";
+}
+console.log(test);
+console.log(test());
+*/
+/*
+var fn = function() {
+	console.log("함수");
+}
+var x = fn;
+x();
+*/
+var interval = setInterval();
