8314445e8f889a58f9b0b693de2631348f00f4e3
Booldook <booldook@gmail.com>
2018-10-16 11:14:00 +0900

    181016-02

--- js/main9.js
+++ js/main9.js
@@ -1,167 +1,31 @@
-/*
-var data = [];
-data[0] = "테스트0";
-data[1] = "테스트1";
-data[2] = "테스트2";
-data[3] = "테스트3";
-data[4] = "테스트4";
-$("#bt").click(function(){
-	var html = '';
-	for(var i=0; i<data.length; i++) {
-		html = '<li>'+data[i]+'</li>';
-		$("#wrap").append(html);
-	}
-});
-*/
-/*
-var data = {
-	title:["테스트0", "테스트1", "테스트2"],
-	link:["#1", "#2", "#3"]
-}
-
-var data2 = [{
-	title:"테스트0",
-	link:"#0"
-},{
-	title:"테스트1",
-	link:"#1"
-},{
-	title:"테스트2",
-	link:"#2"
-}];
-
-$("#bt").click(function(){
-	var html = '';
-	for(var i=0; i<data.title.length; i++){
-		html += '<li><a href="'+data.link[i]+'">'+data.title[i]+'</a></li>';
-	}
-	$("#wrap").append(html);
-
-	var html = '';
-	for(var i=0; i<data2.length; i++) {
-		html += '<li><a href="'+data2[i].link+'">'+data2[i].title+'</a></li>';
-	}
-	$("#wrap").append(html);
-});
-*/
-
-var data = [
-	[{
-		title: "이미지제목0",
-		img: "../img/site0.jpg"
-	}, {
-		title: "이미지제목1",
-		img: "../img/site1.jpg"
-	}],
-	[{
-		title: "이미지제목2",
-		img: "../img/site2.jpg"
-	}, {
-		title: "이미지제목3",
-		img: "../img/site3.jpg"
-	}],
-	[{
-		title: "이미지제목4",
-		img: "../img/site4.jpg"
-	}, {
-		title: "이미지제목5",
-		img: "../img/site5.jpg"
-	}, {
-		title: "이미지제목6",
-		img: "../img/site6.jpg"
-	}]
-];
-//console.log(data[0][1].title);
-
-/*
-$("#bt").click(function(){
-	var html = '';
-	for(var i=0; i<data.length; i++) {
-		html = '<li>';
-		for(var j=0; j<data[i].length; j++) {
-			html += '<ul>';
-			html += '<li><img src="'+data[i][j].img+'" class="img"></li>';
-			html += '<li>'+data[i][j].title+'</li>';
-			html += '</ul>';
-		}
-		html += '</li>';
-		$("#wrap").append(html);
-	}
-});
-*/
-/*
-<li>
-    <ul>
-      <li><img src="../img/site0.jpg" class="img"></li>
-      <li>이미지제목0</li>
-		</ul>
-		<ul>
-      <li><img src="../img/site0.jpg" class="img"></li>
-      <li>이미지제목0</li>
-    </ul>
-</li>
-<li>
-    <ul>
-      <li><img src="../img/site0.jpg" class="img"></li>
-      <li>이미지제목0</li>
-		</ul>
-		<ul>
-      <li><img src="../img/site0.jpg" class="img"></li>
-      <li>이미지제목0</li>
-    </ul>
-</li>
-<li>
-    <ul>
-      <li><img src="../img/site0.jpg" class="img"></li>
-      <li>이미지제목0</li>
-		</ul>
-		<ul>
-      <li><img src="../img/site0.jpg" class="img"></li>
-      <li>이미지제목0</li>
-		</ul>
-		<ul>
-      <li><img src="../img/site0.jpg" class="img"></li>
-      <li>이미지제목0</li>
-    </ul>
-</li>
-*/
-/*
-var data2 = {
-	li:[{
-		ul:[{
-			title: "이미지제목0",
-			img: "../img/site0.jpg"
-		},{
-			title: "이미지제목1",
-			img: "../img/site1.jpg"
-		}]
-	}]
-};
-
-console.log(data2.li[0].ul[1].title);
-*/
-
-$("#bt").click(function(){
+$("#bt_list").click(function () {
 	$.ajax({
-		url:"../json/cate.json",
-		type:"get",
-		dataType:"json",
-		success: function(data){
-			console.log(data);
-			for(var i=0; i<data.result.length; i++){
-				var html  = '<li>';
-				for(var j=0; j<data.result[i].length; j++) {
-					html += '<ul>';
-					html += '<li><img src="'+data.result[i][j].img+'" class="img"></li>';
-					html += '<li>'+data.result[i][j].title+'</li>';
-					html += '</ul>';
+		url: "../json/board.json",
+		type: "get",
+		dataType: "json",
+		data: {
+			page: 1
+		},
+		success: function (data) {
+			if(data.code == 200) {
+				$(".board > tbody").empty();
+				for(var i=0; i<data.result.length; i++){
+					html = '<tr>';
+					html+= '<td>'+data.result[i].no+'</td>';
+					html+= '<td>'+data.result[i].title+'</td>';
+					html+= '<td>'+data.result[i].wdate+'</td>';
+					html+= '<td>'+data.result[i].writer+'</td>';
+					html+= '</tr>';
+					$(".board > tbody").append(html);
 				}
-				html += '</li>';
-				$("#wrap").append(html);
+			}
+			else {
+				alert("데이터가 이상이 있습니다.\n다시 시도해 주세요.");
 			}
 		},
-		error: function(error){
-			console.log(error);
+		error: function (xhr, status, error) {
+			console.log(xhr, status, error);
+			alert("예기치 못한 오류가 발생했습니다.\n다시 시도해 주세요.");
 		}
 	});
 });
