066db323f0ea988d8b92cbcbca996c857af7b782
Booldook <booldook@gmail.com>
2018-10-24 15:33:58 +0900

    181023-04

--- js/ani1.js
+++ js/ani1.js
@@ -18,24 +18,33 @@ function pagerInit(){
 		$(".pager").append(item);
 	});
 };
-(function(){
-	$(".ban").each(function(){
-		$('.pager').append('<a href="'+$(this).data('link')+'" class="w3-bar-item w3-button" onclick="pageSel(this);">'+$(this).data('name')+'</a>');
-	});
-})();
 
-function pageSel(obj) {
-
-}
 
 (function banFadeInOut() {
 	var cnt = $('.ban').length;
 	var dep = 1;
 	var stn = 0;
-	var interval = setInterval(function() {
+	var interval = setInterval(banAni, 4000);
+	$(".ban").each(function(){
+		$('.pager').append('<a href="'+$(this).data('link')+'" class="w3-bar-item w3-button">'+$(this).data('name')+'</a>');
+	});
+	$(".pager > a").click(function(){
+		clearInterval(interval);
+		stn = $(this).index();
+		banAni();
+		interval = setInterval(banAni, 4000);
+	});
+	function banAni() {
 		$(".ban").eq(stn).css({"z-index":dep++, "opacity":0});
 		$(".ban").eq(stn).stop().animate({"opacity":1}, 1000);
+		$(".pager > a").removeClass("w3-red");
+		$(".pager > a").eq(stn).addClass("w3-red");
 		if(stn == cnt-1) stn = 0;
 		else stn++;
-	}, 4000);
+	}
+	$(".ban_wrap").hover(function(){
+		clearInterval(interval);
+	}, function(){
+		interval = setInterval(banAni, 4000);
+	});
 })();
