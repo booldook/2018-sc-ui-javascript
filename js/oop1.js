604b8041df21f25f16a97f4f305420e01daa755b
Booldook <booldook@gmail.com>
2018-10-25 15:26:30 +0900

    181025-06

--- js/oop1.js
+++ js/oop1.js
@@ -28,13 +28,13 @@ var myCar = new Car("붕붕이");
 console.log(myCar, myCar.name);
 */
 
-var Bus = (function(){
-	function Bus(_data) {
+var Box = (function(){
+	function Box(_data) {
 		this.obj = _data;
 		this.create();
 	};
-	Bus.prototype.create = function(){
-		var style = ' style="';
+	Box.prototype.create = function(){
+		var style = ' style="position:relative;';
 		if(this.obj.width) style += 'width:'+this.obj.width+'px;';
 		if(this.obj.height) style += 'height:'+this.obj.height+'px;';
 		if(this.obj.bgColor) style += 'background-color:'+this.obj.bgColor+';';
@@ -43,21 +43,27 @@ var Bus = (function(){
 		if(this.obj.borderColor) style += 'border-color:'+this.obj.borderColor+';';
 		style += '"';
 		var html = '<div style="padding:20px;">';
-		html += '<button class="w3-button w3-red">이동</button> ';
-		html += '<button class="w3-button w3-blue">초기화</button>';
+		html += '<button class="w3-button w3-red bt_move">이동</button> ';
+		html += '<button class="w3-button w3-blue bt_reset">초기화</button>';
 		html += '</div>';
-		html += '<div '+style+'">';
+		this.obj.bts = $(html).appendTo($(this.obj.parent));
+		html = '<div '+style+'">';
 		if(this.obj.name) html += this.obj.name;
 		html += '</div>';
-		this.obj.my = $(html).appendTo($(this.obj.parent));
+		this.obj.box = $(html).appendTo($(this.obj.parent));
 	};
-	Bus.prototype.init = function(){
-
+	Box.prototype.init = function(_target, _speed){
+		$(".bt_move", $(this.obj.bts)).on("click", function(){
+			$(this).parent().next().stop().animate({"left":_target+"px"}, _speed);
+		});
+		$(".bt_reset", $(this.obj.bts)).on("click", function(){
+			$(this).parent().next().stop().animate({"left":"0px"}, _speed);
+		});
 	};
-	return Bus;
+	return Box;
 }());
 
-var bus = new Bus({
+var bus = new Box({
 	parent:".wrap",
 	name: "1500",
 	bgColor: "#f00",
@@ -67,11 +73,13 @@ var bus = new Bus({
 	width: 200,
 	height: 200
 });
+bus.init(1000, 1000);
 
-var bus2 = new Bus({
+var bus2 = new Box({
 	parent: ".wrap",
 	name: "마을버스",
 	bgColor: "#0f0",
 	width:100,
 	height:100
 });
+bus2.init(500, 2000);
