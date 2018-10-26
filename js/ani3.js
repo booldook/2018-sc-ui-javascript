36a9ddbdc633ecefca489c67c90d6aae0ff7d7e8
Booldook <booldook@gmail.com>
2018-10-26 17:31:49 +0900

    181026-07

--- js/ani3.js
+++ js/ani3.js
@@ -1,17 +1,11 @@
 //type normal/pingpong/infinite
 var banner = new Slide($("#slides1"), {
-	type: "fade",
-	delay: 3000,
+	type: "vertical",
+	delay: 2000,
 	speed: 1000
-}); 
-
-/*
-var banner2 = new Slide($("#slides2"), {
-	type: "pingpong",
-	delay: 1500,
-	speed: 300
 });
-*/ 
+
+
 
 
 
--- js/slide.js
+++ js/slide.js
@@ -17,6 +17,11 @@ var Slide = (function(){
 				speed: 300
 			}
 		}
+		$(window).resize(function(){
+			obj.wid = $(obj.slide[0]).width();
+			obj.hei = $(obj.slide[0]).height();
+			obj.slides.height(obj.hei);
+		}).trigger("resize");
 		switch(this.option.type) {
 			case "pingpong" :
 				this.initPingpong();
@@ -27,18 +32,17 @@ var Slide = (function(){
 			case "fade" :
 				this.initFade();
 				break;
+			case "vertical" :
+				this.initVertical();
+				break;
 			default :
 				this.initNormal();
 				break;
 		}
-		$(window).resize(function(){
-			obj.wid = $(obj.slide[0]).width();
-			obj.hei = $(obj.slide[0]).height();
-			obj.slides.height(obj.hei);
-		}).trigger("resize");
 	};
 	//type:pingpong
 	Slide.prototype.initPingpong = function() {
+		console.log(this.slide);
 		for(var i=0; i<this.cnt; i++) {
 			$(this.slide[i]).css({"left":(this.wid*i)+"px"});
 		}
@@ -56,6 +60,7 @@ var Slide = (function(){
 	};
 	//type:infinite
 	Slide.prototype.initInfinite = function() {
+		console.log(this.slide);
 		this.slides.find(".slide").eq(0).clone().appendTo(this.slides);
 		this.slide = $(".slide", this.slides);
 		this.cnt = this.slide.length;
@@ -81,11 +86,33 @@ var Slide = (function(){
 		this.slideFade();
 	};
 	Slide.prototype.slideFade = function(){
+		console.log("fade");
 		var obj = this;
 		this.slide.eq(this.now).css({"z-index":this.depth++, "display":"none"}).delay(this.option.delay).fadeIn(this.option.speed, function(){
 			if(obj.now == obj.cnt - 1) obj.now = 0;
 			else obj.now++;
 			obj.slideFade();
+		});
+	};
+	//type:vertical
+	Slide.prototype.initVertical = function() {
+		this.slides.find(".slide").eq(0).clone().appendTo(this.slides);
+		this.slide = $(".slide", this.slides);
+		this.cnt = this.slide.length;
+		for(var i=0; i<this.cnt; i++) {
+			$(this.slide[i]).css({"top":(this.hei*i)+"px"});
+		}
+		this.slideVertical();
+	};
+	Slide.prototype.slideVertical = function(){
+		var obj = this;
+		this.slides.delay(this.option.delay).animate({"top":-(this.now*this.hei)+"px"}, this.option.speed, function(){
+			if(obj.now == obj.cnt - 1) {
+				obj.slides.css({"top":0});
+				obj.now = 0;
+			}
+			obj.now++;
+			obj.slideVertical();
 		});
 	};
 	//type:normal
