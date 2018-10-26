e9fe012337b267b5c44d54cfbd91eacaa82bd8e0
Booldook <booldook@gmail.com>
2018-10-26 11:59:34 +0900

    181026-04

--- js/ani3.js
+++ js/ani3.js
@@ -1,8 +1,14 @@
 //type normal/pingpong/infinite
 var banner = new Slide($("#slides1"), {
-	type: "pingpong",
+	type: "normal",
 	delay: 1000,
 	speed: 100
+}); 
+
+var banner2 = new Slide($("#slides2"), {
+	type: "pingpong",
+	delay: 1500,
+	speed: 300
 }); 
 
 
