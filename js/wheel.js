c1eac04660b53cc7d81ee3cb9950198599a86fc3
booldook <booldook@gmail.com>
2018-11-22 11:19:41 +0900

    181122-03

--- js/wheel.js
+++ js/wheel.js
@@ -1,24 +1,29 @@
-$(window).on("mousewheel DOMMouseScroll", function(e){
+$(window).on("mousewheel DOMMouseScroll", wheelFn);
+
+function wheelFn(e) {
 	e.preventDefault();
 	e.stopPropagation();
 	var dir = e.originalEvent.wheelDelta;
+	$(window).off("mousewheel DOMMouseScroll");
 	var scTop = $(window).scrollTop();
 	var gap = [];
 	var now = 0;
-	$(".page").each(function(i){
+	$(".page").each(function (i) {
 		gap[i] = $(this).offset().top;
 	});
-	for(var i=0; i<gap.length; i++) {
-		if(scTop <= gap[i]) {
+	for (var i = 0; i < gap.length; i++) {
+		if (scTop <= gap[i]) {
 			now = i;
 			break;
 		}
 	}
-	if(dir > 0) {
-		if(now > 0) now--;
-	}
+	if (dir > 0) {
+		if (now > 0) now--;
+	} 
 	else {
-		if(now < gap.length-1) now++;
+		if (now < gap.length - 1) now++;
 	}
-	$("html, body").stop().delay(100).animate({"scrollTop":gap[now]+"px"}, 200);
-});
+	$("html, body").stop().animate({"scrollTop": gap[now] + "px"}, 200, function(){
+		$(window).on("mousewheel DOMMouseScroll", wheelFn);
+	});
+}
