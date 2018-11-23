7b663574fa3a574075379395ee921be15a7cf448
booldook <booldook@gmail.com>
2018-11-23 12:12:30 +0900

    181123-05

---
+++ js/movie.js
@@ -0,0 +1,17 @@
+$('.movie_bg').tubular({videoId:'_PcE0yjdL48'});
+
+/*
+ratio: 16/9 // usually either 4/3 or 16/9 -- tweak as needed
+videoId: 'ZCAnLxRvNNc' // toy robot in space is a good default, no?
+mute: true
+repeat: true
+width: $(window).width() // no need to override
+wrapperZIndex: 99
+playButtonClass: 'tubular-play'
+pauseButtonClass: 'tubular-pause'
+muteButtonClass: 'tubular-mute'
+volumeUpClass: 'tubular-volume-up'
+volumeDownClass: 'tubular-volume-down'
+increaseVolumeBy: 10 // increment value; volume range is 1-100
+start: 0 // starting position in seconds
+*/
