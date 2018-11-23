7128bda3c82778eb8791184af49b231f2466b058
booldook <booldook@gmail.com>
2018-11-23 10:55:20 +0900

    181123-01

---
+++ js/chart.js
@@ -0,0 +1,54 @@
+var data = [{
+	datasets: [{
+			data: [70, 30],
+			backgroundColor: [
+				'rgba(255, 99, 132, 0.5)',
+				'rgba(255, 255, 255, 0)'
+			]
+	}]
+},{
+	labels: ["Red", "Blue", "Yellow"],
+	datasets: [{
+			data: [12, 19, 3],
+			backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)'
+			],
+			borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)'
+			],
+			borderWidth: 1
+	}]
+},{
+	labels: ["Red", "Blue", "Yellow"],
+	datasets: [{
+			data: [12, 19, 3],
+			backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)'
+			],
+			borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)'
+			],
+			borderWidth: 1
+	}]
+}];
+var option = [{},{},{}];
+var chart = [];
+
+var ctx = $(".chart");
+ctx.each(function(i){
+	chart[i] = new Chart($(this), {
+		type: 'doughnut',
+		data: data[i],
+		option: option[i]
+	});
+});
+
+
